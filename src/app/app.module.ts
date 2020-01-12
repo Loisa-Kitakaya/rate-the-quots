3fae6a3a5e1b4805f985430ca4f7e2bdfbd7ddc5
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-12 09:29:01 +0300

    register routes for application navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { QuotesDashboardComponent } from './quotes-dashboard/quotes-dashboard.component';
-import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { QuotesDashboardComponent } from "./quotes-dashboard/quotes-dashboard.component";
+import { QuotesFormComponent } from "./quotes-form/quotes-form.component";
+
+//registering routes
+const appRoutes: Routes = [
+  { path: "post-quotes", component: QuotesFormComponent },
+  { path: "dashboard", component: QuotesDashboardComponent },
+  { path: "", redirectTo: "/dashboard", pathMatch: "full" }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    QuotesDashboardComponent,
-    QuotesFormComponent
-  ],
+  declarations: [AppComponent, QuotesDashboardComponent, QuotesFormComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
