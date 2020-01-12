71582ab1bb06a7bb4015a35ed7f54b911cb441d2
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-12 14:18:45 +0300

    added properties: upvote and downvote to beused in the quote voting process

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,7 +1,10 @@
 export class Quotes {
   //creating a constructor with required properties
   showQuote: boolean;
+
   constructor(
+    public upVote: number = 0,
+    public downVote: number = 0,
     public userName: string,
     public quoteTitle: string,
     public quoteAuthor: string,
