04377f9332b732b6419694fa4b59fd4c57a0e5d1
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-12 10:41:01 +0300

    created an object and initialized it with instances

--- src/app/quotes-dashboard/quotes-dashboard.component.ts
+++ src/app/quotes-dashboard/quotes-dashboard.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Quotes } from "../quotes";
 
 @Component({
-  selector: 'app-quotes-dashboard',
-  templateUrl: './quotes-dashboard.component.html',
-  styleUrls: ['./quotes-dashboard.component.css']
+  selector: "app-quotes-dashboard",
+  templateUrl: "./quotes-dashboard.component.html",
+  styleUrls: ["./quotes-dashboard.component.css"]
 })
 export class QuotesDashboardComponent implements OnInit {
+  //creating an object and initializing it
+  quotes: Quotes[] = [
+    new Quotes("loisa", "einstein", "he said some things about physics"),
+    new Quotes("ship ship", "riri", "she said something about fashion"),
+    new Quotes("sim sim", "jay-z", "he said something about music"),
+    new Quotes("sim sim", "jay-z", "he said something about music"),
+    new Quotes("sim sim", "jay-z", "he said something about music")
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
