9ea3f22d68e78e2a042f35b0b6db0cab690edb4f
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-12 14:20:15 +0300

    created functions to vote for quotes and bound them to the dashboard.html component

--- src/app/quotes-dashboard/quotes-dashboard.component.ts
+++ src/app/quotes-dashboard/quotes-dashboard.component.ts
@@ -9,11 +9,38 @@ import { Quotes } from "../quotes";
 export class QuotesDashboardComponent implements OnInit {
   //creating an object and initializing it
   quotes: Quotes[] = [
-    new Quotes("loisa", "einstein", "he said some things about physics"),
-    new Quotes("ship ship", "riri", "she said something about fashion"),
-    new Quotes("sim sim", "jay-z", "he said something about music"),
-    new Quotes("sim sim", "jay-z", "he said something about music"),
-    new Quotes("sim sim", "jay-z", "he said something about music")
+    new Quotes(
+      0,
+      0,
+      "loisa",
+      "e=mc2",
+      "einstein",
+      "he said some things about physics"
+    ),
+    new Quotes(
+      0,
+      0,
+      "ship ship",
+      "sivir",
+      "riri",
+      "she said something about fashion"
+    ),
+    new Quotes(
+      0,
+      0,
+      "sim 1",
+      "tot",
+      "jay-y",
+      "he said somethiljsdflkjsdng about music"
+    ),
+    new Quotes(
+      0,
+      0,
+      "sim 2",
+      "vee",
+      "jay-x",
+      "he said something ablsjdfpsdfkout music"
+    )
   ];
 
   //showing and hiding quotes
@@ -25,8 +52,25 @@ export class QuotesDashboardComponent implements OnInit {
   deleteQuote(index) {
     let deleteThis = confirm("Are you sure you want to delete quote: ");
 
-    if(deleteThis) {
+    if (deleteThis) {
       this.quotes.splice(index, 1);
+    }
+  }
+
+  //voting for the quotes
+  public likeQuote: boolean = true;
+
+  like(index) {
+    if (this.likeQuote) {
+      this.quotes[index].upVote += 1;
+    }
+  }
+
+  public dislikeQuote: boolean = true;
+
+  dislike(index) {
+    if (this.dislikeQuote) {
+      this.quotes[index].downVote += 1;
     }
   }
 
