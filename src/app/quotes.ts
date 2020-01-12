b3cfe55fadad3a0203cb9d7514e2e545419b2c1c
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-12 09:47:36 +0300

    created a constructor which holds the required properties

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,7 @@
 export class Quotes {
+  //creating a constructor with required properties
+  showQuote: boolean;
+  constructor(public userName: string, public quoteAuthor: string, public quote: string) {
+    this.showQuote = false;
+  }
 }
