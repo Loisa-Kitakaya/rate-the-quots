bd97cc44a0305ee2df042752d09de2be7ce999bd
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-12 12:28:19 +0300

    added a property to deal with the title of the quotes

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,7 +1,12 @@
 export class Quotes {
   //creating a constructor with required properties
   showQuote: boolean;
-  constructor(public userName: string, public quoteAuthor: string, public quote: string) {
+  constructor(
+    public userName: string,
+    public quoteTitle: string,
+    public quoteAuthor: string,
+    public quote: string
+  ) {
     this.showQuote = false;
   }
 }
