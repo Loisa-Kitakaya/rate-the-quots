5455275a0972fa15f448a1b28cdbf361759a28df
Loisa-Kitakaya <loisakitakaya@gmail.com>
2020-01-12 08:12:15 +0300

    import components: 'QuotesDashboardComponent', 'QuotesFormComponent' such that they are usable throughout the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesDashboardComponent } from './quotes-dashboard/quotes-dashboard.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesDashboardComponent,
+    QuotesFormComponent
   ],
   imports: [
     BrowserModule,
